65d9cb41396787e3e0e482552f157d57905957e5
 temp <temp@Nogahs-MacBook-Pro-2.local>
2019-11-03 10:29:04 +0200

    new project

---
+++ OpenMovieDB/OpenMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenMovieDB
+//
+//  Created by  temp on 03/11/2019.
+//  Copyright © 2019  temp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OpenMovieDB/OpenMovieDBTests/OpenMovieDBTests.swift
@@ -0,0 +1,34 @@
+//
+//  OpenMovieDBTests.swift
+//  OpenMovieDBTests
+//
+//  Created by  temp on 03/11/2019.
+//  Copyright © 2019  temp. All rights reserved.
+//
+
+import XCTest
+@testable import OpenMovieDB
+
+class OpenMovieDBTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
