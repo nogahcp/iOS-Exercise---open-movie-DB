ce7c5f0776b10bc27565fa801bc73ddc156849ff
 temp <temp@Nogahs-MacBook-Pro-2.local>
2019-11-04 16:05:37 +0200

    movie details is shown (except poster)

--- OpenMovieDB/OpenMovieDB/MovieDetailsViewController.swift
+++ OpenMovieDB/OpenMovieDB/MovieDetailsViewController.swift
@@ -8,26 +8,38 @@
 
 import UIKit
 
-class MovieDetailsViewController: UIViewController {
+class MovieDetailsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     var movieDBModel = MoviesDBModel()
 
+    @IBOutlet weak var movieInfoCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //get movie details from model
-        
+        self.movieInfoCollectionView.delegate = self
+        self.movieInfoCollectionView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //get number of cells from model
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.movieDBModel.details.count
     }
-    */
+    
+    //create cell and return
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = self.movieInfoCollectionView.dequeueReusableCell(withReuseIdentifier: "movieDetailCell", for: indexPath) as! MovieInfoCollectionViewCell
+        //get param and info
+        let currParam = Array(self.movieDBModel.details.keys)[indexPath.row]
+        cell.paramLabel.text = currParam
+        
+        guard let currInfo = self.movieDBModel.details[currParam] as? String
+            else {
+                return cell
+                }
+        cell.infoLabel.text = currInfo
+        return cell
+    }
 
 }
---
+++ OpenMovieDB/OpenMovieDB/MovieInfoCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieInfoCollectionViewCell.swift
+//  OpenMovieDB
+//
+//  Created by  temp on 04/11/2019.
+//  Copyright © 2019  temp. All rights reserved.
+//
+
+import UIKit
+
+class MovieInfoCollectionViewCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
+    @IBOutlet weak var paramLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+}
